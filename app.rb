40c48f608e0de2ca708954575415935b2306a52f
Balint Erdi <balint.erdi@gmail.com>
2013-10-17 18:29:53 +0200

    Endpoint for getting an individual artist with its songs

--- app.rb
+++ app.rb
@@ -24,3 +24,23 @@ post '/artists.json' do
   { artist: attributes.merge(id: artist_id) }.to_json
 end
 
+get '/artists/:slug.json' do
+  artists = DB[:artists]
+  artist = artists.detect do |artist|
+    name = artist[:name]
+    name_parts = name.split(/\s+/).map(&:downcase)
+    artist_slug = name_parts.join('-')
+    artist_slug == params[:slug]
+  end
+  artist_songs_records = DB[:songs].where(artist_id: artist[:id])
+  artist_songs = [].tap do |songs|
+    artist_songs_records.each do |song|
+      songs << { id: song[:id], title: song[:title], rating: song[:rating] }
+    end
+  end
+  {
+    name: artist[:name],
+    songs: artist_songs
+  }.to_json
+end
+
